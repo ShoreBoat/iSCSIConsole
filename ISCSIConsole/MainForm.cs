3e76f3e4a962796e6172f7c0b122eb1be6ec8d4a
shoreboat <61381286+ShoreBoat@users.noreply.github.com>
2026-08-23 03:36:21 +0800

    Restore targets and start iSCSI automatically

--- ISCSIConsole/MainForm.cs
+++ ISCSIConsole/MainForm.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
@@ -25,16 +26,23 @@ namespace ISCSIConsole
         private List<ISCSITarget> m_targets = new List<ISCSITarget>();
         private UsageCounter m_usageCounter = new UsageCounter();
         private bool m_started = false;
+        private bool m_restoringConfiguration = false;
+        private bool m_autoStartLaunch = false;
 
-        public MainForm()
+        public MainForm() : this(false)
         {
+        }
+
+        public MainForm(bool autoStartLaunch)
+        {
+            m_autoStartLaunch = autoStartLaunch;
             InitializeComponent();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-            this.Text += " v" + version.ToString(3);
+            this.Text += " v" + version.ToString(3) + " AutoStart";
             m_server.OnLogEntry += Program.OnLogEntry;
 
             List<IPAddress> localIPs = GetHostIPAddresses();
@@ -47,11 +55,21 @@ namespace ISCSIConsole
             comboIPAddress.DataSource = list;
             comboIPAddress.DisplayMember = "Key";
             comboIPAddress.ValueMember = "Value";
-            lblStatus.Text = "Author: Tal Aloni (tal.aloni.il@gmail.com)";
+            lblStatus.Text = "Author: Tal Aloni (AutoStart build)";
 
             if (RuntimeHelper.IsWin32 && !SecurityHelper.IsAdministrator())
             {
-                lblStatus.Text = "Some features require administrator privileges and have been disabled";
+                lblStatus.Text = "Run as administrator once to register Windows startup";
+            }
+
+            RestoreConfiguration();
+
+            if (m_autoStartLaunch)
+            {
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                });
             }
         }
 
@@ -59,28 +77,11 @@ namespace ISCSIConsole
         {
             if (!m_started)
             {
-                IPAddress serverAddress = (IPAddress)comboIPAddress.SelectedValue;
-                int port = Conversion.ToInt32(txtPort.Text, 0);
-                if (port <= 0 || port > UInt16.MaxValue)
+                if (StartServer())
                 {
-                    MessageBox.Show("Invalid TCP port", "Error");
-                    return;
+                    SaveConfiguration(true);
+                    RegisterWindowsStartup();
                 }
-                IPEndPoint endpoint = new IPEndPoint(serverAddress, port);
-                try
-                {
-                    m_server.Start(endpoint);
-                }
-                catch (SocketException ex)
-                {
-                    MessageBox.Show("Cannot start server, " + ex.Message, "Error");
-                    return;
-                }
-                btnStart.Text = "Stop";
-                txtPort.Enabled = false;
-                comboIPAddress.Enabled = false;
-                m_started = true;
-                UpdateUI();
             }
             else
             {
@@ -90,7 +91,37 @@ namespace ISCSIConsole
                 btnStart.Text = "Start";
                 txtPort.Enabled = true;
                 comboIPAddress.Enabled = true;
+                SaveConfiguration(false);
             }
+        }
+
+        private bool StartServer()
+        {
+            IPAddress serverAddress = (IPAddress)comboIPAddress.SelectedValue;
+            int port = Conversion.ToInt32(txtPort.Text, 0);
+            if (port <= 0 || port > UInt16.MaxValue)
+            {
+                MessageBox.Show("Invalid TCP port", "Error");
+                return false;
+            }
+
+            IPEndPoint endpoint = new IPEndPoint(serverAddress, port);
+            try
+            {
+                m_server.Start(endpoint);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show("Cannot start server, " + ex.Message, "Error");
+                return false;
+            }
+
+            btnStart.Text = "Stop";
+            txtPort.Enabled = false;
+            comboIPAddress.Enabled = false;
+            m_started = true;
+            UpdateUI();
+            return true;
         }
 
         private void btnAddTarget_Click(object sender, EventArgs e)
@@ -100,21 +131,32 @@ namespace ISCSIConsole
             if (addTargetResult == DialogResult.OK)
             {
                 ISCSITarget target = addTarget.Target;
-                ((SCSI.VirtualSCSITarget)target.SCSITarget).OnLogEntry += Program.OnLogEntry;
-                target.OnAuthorizationRequest += new EventHandler<AuthorizationRequestArgs>(ISCSITarget_OnAuthorizationRequest);
-                target.OnSessionTermination += new EventHandler<SessionTerminationArgs>(ISCSITarget_OnSessionTermination);
-                m_targets.Add(target);
-                try
+                if (RegisterTarget(target))
                 {
-                    m_server.AddTarget(target);
+                    SaveConfiguration(m_started);
                 }
-                catch (ArgumentException ex)
-                {
-                    MessageBox.Show(ex.Message, "Error");
-                    return;
-                }
-                listTargets.Items.Add(target.TargetName);
             }
+        }
+
+        private bool RegisterTarget(ISCSITarget target)
+        {
+            ((SCSI.VirtualSCSITarget)target.SCSITarget).OnLogEntry += Program.OnLogEntry;
+            target.OnAuthorizationRequest += new EventHandler<AuthorizationRequestArgs>(ISCSITarget_OnAuthorizationRequest);
+            target.OnSessionTermination += new EventHandler<SessionTerminationArgs>(ISCSITarget_OnSessionTermination);
+
+            try
+            {
+                m_server.AddTarget(target);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                return false;
+            }
+
+            m_targets.Add(target);
+            listTargets.Items.Add(target.TargetName);
+            return true;
         }
 
         private void btnRemoveTarget_Click(object sender, EventArgs e)
@@ -133,6 +175,188 @@ namespace ISCSIConsole
                 LockUtils.ReleaseDisks(disks);
                 m_targets.RemoveAt(targetIndex);
                 listTargets.Items.RemoveAt(targetIndex);
+                SaveConfiguration(m_started);
+            }
+        }
+
+        private void RestoreConfiguration()
+        {
+            AutoStartConfiguration configuration;
+            try
+            {
+                configuration = AutoStartConfigurationStore.Load();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot load AutoStart configuration: " + ex.Message, "AutoStart");
+                return;
+            }
+
+            if (configuration == null)
+            {
+                return;
+            }
+
+            m_restoringConfiguration = true;
+            try
+            {
+                txtPort.Text = configuration.Port.ToString();
+
+                IPAddress configuredAddress;
+                if (IPAddress.TryParse(configuration.IPAddress, out configuredAddress))
+                {
+                    bool addressAvailable = configuredAddress.Equals(IPAddress.Any);
+                    if (!addressAvailable)
+                    {
+                        foreach (IPAddress localAddress in GetHostIPAddresses())
+                        {
+                            if (localAddress.Equals(configuredAddress))
+                            {
+                                addressAvailable = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (addressAvailable)
+                    {
+                        comboIPAddress.SelectedValue = configuredAddress;
+                    }
+                    else
+                    {
+                        comboIPAddress.SelectedValue = IPAddress.Any;
+                        lblStatus.Text = "Saved IP is unavailable; listening on Any address";
+                    }
+                }
+
+                if (configuration.Targets != null)
+                {
+                    foreach (TargetConfiguration targetConfiguration in configuration.Targets)
+                    {
+                        RestoreTarget(targetConfiguration);
+                    }
+                }
+
+                if (configuration.StartServerOnLaunch && m_targets.Count > 0)
+                {
+                    StartServer();
+                }
+            }
+            finally
+            {
+                m_restoringConfiguration = false;
+            }
+        }
+
+        private void RestoreTarget(TargetConfiguration targetConfiguration)
+        {
+            List<Disk> disks = new List<Disk>();
+            try
+            {
+                if (targetConfiguration.DiskImagePaths == null || targetConfiguration.DiskImagePaths.Count == 0)
+                {
+                    return;
+                }
+
+                foreach (string path in targetConfiguration.DiskImagePaths)
+                {
+                    if (!File.Exists(path))
+                    {
+                        throw new FileNotFoundException("Disk image not found", path);
+                    }
+
+                    DiskImage diskImage = DiskImage.GetDiskImage(path, false);
+                    bool isLocked = false;
+                    try
+                    {
+                        isLocked = diskImage.ExclusiveLock();
+                    }
+                    catch (IOException)
+                    {
+                    }
+
+                    if (!isLocked)
+                    {
+                        throw new IOException("Cannot lock disk image for exclusive access: " + path);
+                    }
+                    disks.Add(diskImage);
+                }
+
+                ISCSITarget target = new ISCSITarget(targetConfiguration.IQN, disks);
+                if (!RegisterTarget(target))
+                {
+                    LockUtils.ReleaseDisks(disks);
+                }
+            }
+            catch (Exception ex)
+            {
+                LockUtils.ReleaseDisks(disks);
+                MessageBox.Show("Cannot restore target " + targetConfiguration.IQN + ": " + ex.Message, "AutoStart");
+            }
+        }
+
+        private void SaveConfiguration(bool startServerOnLaunch)
+        {
+            if (m_restoringConfiguration)
+            {
+                return;
+            }
+
+            AutoStartConfiguration configuration = new AutoStartConfiguration();
+            configuration.StartServerOnLaunch = startServerOnLaunch;
+            IPAddress selectedAddress = comboIPAddress.SelectedValue as IPAddress;
+            configuration.IPAddress = selectedAddress == null ? IPAddress.Any.ToString() : selectedAddress.ToString();
+            configuration.Port = Conversion.ToInt32(txtPort.Text, 3260);
+
+            foreach (ISCSITarget target in m_targets)
+            {
+                TargetConfiguration targetConfiguration = new TargetConfiguration();
+                targetConfiguration.IQN = target.TargetName;
+
+                List<Disk> disks = ((SCSI.VirtualSCSITarget)target.SCSITarget).Disks;
+                bool supported = true;
+                foreach (Disk disk in disks)
+                {
+                    DiskImage diskImage = disk as DiskImage;
+                    if (diskImage == null)
+                    {
+                        supported = false;
+                        break;
+                    }
+                    targetConfiguration.DiskImagePaths.Add(diskImage.Path);
+                }
+
+                if (supported && targetConfiguration.DiskImagePaths.Count > 0)
+                {
+                    configuration.Targets.Add(targetConfiguration);
+                }
+            }
+
+            try
+            {
+                AutoStartConfigurationStore.Save(configuration);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot save AutoStart configuration: " + ex.Message, "AutoStart");
+            }
+        }
+
+        private void RegisterWindowsStartup()
+        {
+            if (!RuntimeHelper.IsWin32)
+            {
+                return;
+            }
+
+            string error;
+            if (!StartupTaskManager.EnsureInstalled(out error))
+            {
+                lblStatus.Text = "iSCSI started, but Windows startup registration failed";
+                MessageBox.Show(
+                    "The server is running, but Windows startup registration failed.\r\n" +
+                    "Run iSCSIConsole as administrator and click Start again.\r\n\r\n" + error,
+                    "AutoStart");
             }
         }
 
@@ -185,7 +409,7 @@ namespace ISCSIConsole
                 IPInterfaceProperties ipProperties = netInterface.GetIPProperties();
                 foreach (UnicastIPAddressInformation addressInfo in ipProperties.UnicastAddresses)
                 {
-                    if (addressInfo.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    if (addressInfo.Address.AddressFamily == AddressFamily.InterNetwork)
                     {
                         result.Add(addressInfo.Address);
                     }
